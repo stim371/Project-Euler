b604dfcf91338972abc444752ce677fe7b9b955f
Joel Stimson <stimsojr@gmail.com>
2011-05-04 22:58:48 -0700

    update 1 and add 2

--- Euler 1/euler1.rb
+++ Euler 1/euler1.rb
@@ -3,23 +3,24 @@
 
 #find the sum of all the multiples of 3 or 5 below 1000
 
-class Naturalsum
+#class Naturalsum
 
-#puts "what value do you want to check?"
-#arrmax = 0
-#arrmax = gets.chomp.to_i
+puts "what value do you want to check?"
+arrmax = 0
+arrmax = gets.chomp.to_i
 
-def naturalsum(arrmax)
+#def naturalsum(arrmax)
 b = []
 
-(1..arrmax).each do |i|
+(1...arrmax).each do |i|
   b<<i if i % 3 == 0 || i % 5 == 0
 end
 
 if b.size == 0
-  return 0
+  puts 0
 else 
-  return b.inject(:+)
+  sum = 0
+  puts b.inject{|sum,x| sum +x }
 end
-end
-end
+#end
+#end
